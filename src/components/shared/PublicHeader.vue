9de3a036e5edeb62b2576c5ee4e4dad76bea3768
Rodrigo Gonçalves <goncalvesrodrigo.rg@gmail.com>
2025-11-19 12:15:49 +0000

    feat: menu mobile

--- src/components/shared/PublicHeader.vue
+++ src/components/shared/PublicHeader.vue
@@ -2,32 +2,33 @@
 	import logo from '../../assets/imgs/logo.svg';
 	import { publicNavigationItems } from '@/constants/PublicNavigation.js';
 import Button from '../ui/Button.vue';
+import MenuButton from './PublicHeader/MenuButton.vue';
 </script>
 
 <template>
-	<header class="px-8">
-		<div class="flex items-center gap-6 max-w-[1120px] m-auto py-5">
+	<header class="px-4 lg:px-8 fixed top-0 left-0 w-full z-200 bg-zinc-950">
+		<div class="flex items-center gap-6 max-w-5xl m-auto py-4.5">
 			<div class="w-full">
 				<router-link to="/">
 					<img :src="logo" alt="logo" class="select-none" />
 				</router-link>
 			</div>
-			<nav>
+			<nav class="hidden lg:block">
 				<ul class="flex items-center gap-6">
 					<li v-for="item in publicNavigationItems" :key="item.id">
 						<router-link
 							:to="item.route"
-							class="text-center font-medium text-sm text-zinc-400 hover:text-white ease-out duration-200">
+							class="text-center font-medium text-sm text-zinc-400 hover:text-white ease-out duration-200 select-none">
 							{{ item.label }}
 						</router-link>
 					</li>
 				</ul>
 			</nav>
-			<div class="w-full flex gap-2 justify-end">
+			<div class="w-full gap-2 justify-end hidden lg:flex">
 				<Button variant="secondary" to="/log-in">Log In</Button>
 				<Button to="/get-started">Get started</Button>
 			</div>
-			<NavigationLayout />
+			<MenuButton />
 		</div>
 	</header>
 </template>
---
+++ src/components/shared/PublicHeader/MenuButton.vue
@@ -0,0 +1,21 @@
+<script></script>
+
+<template>
+	<button
+		class="lg:hidden p-1.5 grid place-content-center hover:bg-zinc-900 rounded-full ease-out duration-200
+		hover:inset-shadow-[0_1px_0_0] hover:inset-shadow-zinc-800 hover:shadow-[0_2px_4px_0] hover:shadow-black"
+	>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+		>
+			<path d="M4 8.5H20" stroke="white" stroke-width="1.5" stroke-linejoin="round" />
+			<path d="M4 15.5H20" stroke="white" stroke-width="1.5" stroke-linejoin="round" />
+		</svg>
+	</button>
+	<!-- M6 18L18 6 -->
+	<!-- M6 6L17.9996 17.9996 -->
+</template>
